a4af1b911c57a0fee6e83368d129546af96730cb
Andrew Hughes <andrew.hughes@gofluid.org>
2021-06-01 13:33:12 +0100

    add footer

---
+++ components/footer.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="footer">
+    <div class="container">
+      <div class="logo">
+        <img src="/img/PlusGroupLogo.svg" />
+      </div>
+      <div class="comp-info">Plus Partner Services Limited, trading as Plus Group. VAT Number 191887360.</div>
+      <div class="links">
+        <router-link to="/">How it works</router-link>
+        <router-link to="/pricing">Pricing</router-link>
+        <router-link to="/faqs">FAQs</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.footer {
+  padding: 20px;
+  background: #f2f2f2;
+  font-size: 12px;
+}
+.footer .container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.footer img {
+  height: 50px;
+}
+.links {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+.links a {
+  text-decoration: none;
+  color: #242424;
+  transition: 75ms ease-in;
+}
+.links a:hover {
+  color: #ed0874;
+}
+</style>
